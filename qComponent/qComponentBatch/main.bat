5abce64101310f8aff18f69413fbfd891d5bf689
DomeniGeco <domenigeco@gmail.com>
2026-03-31 21:23:19 +0200

    Create main.bat

---
+++ qComponent/qComponentBatch/main.bat
@@ -0,0 +1,15 @@
+:: init set
+@echo off
+title qComponentBatch
+
+:: titolo
+echo copyright (c) qComponent
+echo.
+echo ============================
+echo      qComponentBatch
+echo ============================
+echo.
+
+:: avvio
+shell\exec.bat
+
